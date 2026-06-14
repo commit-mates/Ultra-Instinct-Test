e315126742d15616e803c20127d83138fa2b797a
yateesh-chandra-duggirala <yateeshchandraduggirala3@gmail.com>
2026-06-14 18:58:01 +0000

    Scraping the data from freshersworld is completed

--- data_pipelines/bronze/incremental_load/Load_Freshersworld_Feed.py
+++ data_pipelines/bronze/incremental_load/Load_Freshersworld_Feed.py
@@ -44,47 +44,67 @@ headers = {
         "Chrome/125.0.0.0 Safari/537.36"
     )
 }
+
 # Define the Empty List
 payload_list = []
+job_list = []
+
+# COMMAND ----------
+
+# DBTITLE 1,Define a function to get list of Job URLs
+def get_freshersworld_job_urls(url, num):
+    response = requests.get(f"{url}?&offset={num}", headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    jobs = []
+    for card in soup.select(
+        "div.job-container, div.job-card, div.latest-jobs, div.job-new-title"
+    ):
+        job_url = card.get("job_display_url")
+        if job_url:
+            jobs.append(job_url)
+
+    return jobs
 
 # COMMAND ----------
 
 # DBTITLE 1,Define a function to Fetch response from Freshersworld web
-def fetch_freshersworld_jobs(url, num):
-    response = requests.get(f"{url}?&offset={num}", headers=headers)
-    response.raise_for_status()
+def fetch_freshersworld_jobs(job_list):
+    for URL in job_list :
+        response = requests.get(URL, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    soup = BeautifulSoup(response.text, "html.parser")
+        job_id = URL.split("-")[-1]
+        title = soup.select_one(".job-role h2")
+        company = soup.select_one(".company-name span")
+        job_desc = soup.select_one(".job-desc")
+        skills = soup.select_one(".skills-detail")
+        location = soup.select_one(".job-location a")
+        experience = soup.select_one("div.job-details-sub-div span.space")
+        salary = soup.select_one(" div.salary div.job-details-sub-div span.space")
 
-    for card in soup.select("div.job-container, div.job-card, div.latest-jobs, div.job-new-title"):  
-        title = card.select_one(".job-new-title")
-        company = card.select_one(".company-name")
-        location = card.select_one("job-location")
-        experience = card.select_one(".experience")
         payload = {
-            "job_id": card.get("job_id"),
-            "job_url" : card.get("job_display_url"),
+            "job_id": job_id,
+            "job_url" : URL,
             "title": title.get_text(strip=True) if title else None,
+            "job_desc" : job_desc.get_text(separator="\n", strip=True) if job_desc else None,
             "company": company.get_text(strip=True) if company else None,
             "location": location.get_text(strip=True) if location else None,
             "experience": experience.get_text(strip=True) if experience else None,
+            "salary" : salary.get_text(strip=True) if salary else None,
+            "skills": skills.get_text(separator=", ", strip=True) if skills else None
         }
+
         payload_list.append(json.dumps(payload))
-
     return payload_list
-
-# COMMAND ----------
-
-# DBTITLE 1,Used for testing
-num = 0
-val = [(x,) for x in fetch_freshersworld_jobs(URL, num)]
-print(val)
 
 # COMMAND ----------
 
 # DBTITLE 1,Call the Function
 for num in range(0, 101, 20):
-    val = [(x,) for x in fetch_freshersworld_jobs(URL, num)]
+    job_list.extend(get_freshersworld_job_urls(URL, num))
+
+val = [(x,) for x in fetch_freshersworld_jobs(job_list)]
 
 # COMMAND ----------
 
@@ -96,4 +116,4 @@ freshersworld_df = spark.createDataFrame(val,schema = ['PAYLOAD']) \
 # COMMAND ----------
 
 # DBTITLE 1,Appending the data into Target Table
-freshersworld_df.write.mode("append").saveAsTable("jobsintel.bronze.raw_freshersworld_jobs")
+# freshersworld_df.write.mode("append").saveAsTable("jobsintel.bronze.raw_freshersworld_jobs")
